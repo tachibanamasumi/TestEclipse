53082081c269b0b47fa76468720f587db27e46c0
tachibanamasumi <121288558+tachibanamasumi@users.noreply.github.com>
2022-12-27 20:31:29 +0900

    リモートリポジトリで内容変更（println追加）

--- TestGit/src/test/GitTest.java
+++ TestGit/src/test/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
-		System.out.println("リモートリポジトリでHello GitHub!");
+		System.out.println("リモートリポジトリで内容変更");
 	}
 
 }
